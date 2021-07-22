84baed87ce23dd30705251020b1dd3be8dc02584
Jurss <remi62120@hotmail.fr>
2021-07-22 17:35:32 +0200

    Add Page 404

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import './component/CSS/errorPage.css'
+import Error from './img/404.svg';
 
 const ErrorPage = () => {
     return (
-        <div>
+        <div className="mainErrorContainer">
             <h1>Error 404</h1>
+            <h2>Page Not Found</h2>
+            <img src={Error} alt="404" />
         </div>
     )
 }
