8f59b371ad98710efc3d103a69e18e38e9ffabdd
Jurss <remi62120@hotmail.fr>
2021-08-06 08:54:10 +0200

    add constants file for APi key

---
+++ src/constantes.js
@@ -0,0 +1,14 @@
+const arrKey = [
+    '777a242ba0684136ba5af8b964599a11',
+    'eb4f9cbd69184fefaf91e11d2a0e2814',
+    'fc143844671c4e7dbfd946ac2398da37',
+    '4ff1faa944a14d389e56ccb9ef80f238',
+    '394d4e2e504b4f9699e2a95aaa339b66',
+    'a7d5b6ad49f7465db277ac5be93f0f4f',
+    'e15e2fad32d44728989cf080d966481a',
+    'c8989ccb63cf4f3f97c3e05bb1d14ce1'
+]
+
+
+
+export let API_KEY = arrKey[0];
